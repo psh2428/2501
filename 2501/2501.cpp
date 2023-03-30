04708c330a9285c96355787319efdd1bf6f2546a
psh2428 <simon2428@naver.com>
2023-03-30 20:42:15 +0900

    초기 커밋.

---
+++ 2501/2501.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINRGS
+#include <stdio.h>
+
+void main() {
+	int n, k, i;
+	int cnt = 0;
+	scanf_s("%d %d", &n, &k);
+
+	for (i = 1; i <= n; i++) {
+		if (n % i == 0) {
+			cnt++;
+			if (cnt == k)
+				break;
+		
+		}
+	}
+	if (i <= n)
+		printf("%d", i);
+	else 
+		printf("0");
+	
+}
